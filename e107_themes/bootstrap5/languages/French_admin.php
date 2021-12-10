5a613dbbb482308c332aa06bfc918c9904bcea80
leo <the_guardian2@hotmail.com>
2021-12-10 11:14:57 +0100

    v 2.3.1

---
+++ e107_themes/bootstrap5/languages/French_admin.php
@@ -0,0 +1,11 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - 
+|
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEMEPREF_01", "Styles Bootswatch :");
+define("LAN_THEMEPREF_02", "Afficher le menu standard avec l'aspect d'une carte");
+define("LAN_THEMEPREF_03", "Afficher la page de connexion comme page standard");
